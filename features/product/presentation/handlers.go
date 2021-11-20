39f3aee639270000c55d13150947e9f7265ea46e
alvinsatria31 <alvin.satria@konsultan.belajar.id>
2021-11-20 23:39:52 +0700

    add handlers, modified dto

--- features/product/presentation/handlers.go
+++ features/product/presentation/handlers.go
@@ -1,4 +1,23 @@
 package presentation
 
+import (
+	"A-Golang-MiniProject/features/product"
+	"net/http"
+
+	presentation_response "A-Golang-MiniProject/features/product/presentation/response"
+
+	"github.com/labstack/echo/v4"
+)
+
 type ProductHandler struct {
+	productBussiness product.Bussiness
+}
+
+func (ph *ProductHandler) GetAllData(c echo.Context) error {
+	result := ph.productBussiness.GetAllData("")
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "All Data Printed",
+		"data":    presentation_response.FromCoreSlice(result),
+	})
 }
--- features/product/presentation/response/dto.go
+++ features/product/presentation/response/dto.go
@@ -1,12 +1,35 @@
 package response
 
-import "time"
+import (
+	"A-Golang-MiniProject/features/product"
+	"time"
+)
 
 type Product struct {
-	ID        int       `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Name      string    `json:"name"`
-	Price     float32   `json:"price"`
-	Satuan    int       `json:"satuan"`
+	ID          int                   `json:"id"`
+	CreatedAt   time.Time             `json:"created_at"`
+	UpdatedAt   time.Time             `json:"updated_at"`
+	Name        string                `json:"name"`
+	Price       float32               `json:"price"`
+	Satuan      int                   `json:"satuan"`
+	Distributor []product.Distributor `json:"Distributor"`
+}
+
+func FromCore(core product.Core) Product {
+	return Product{
+		ID:        core.ID,
+		CreatedAt: core.CreatedAt,
+		UpdatedAt: core.UpdatedAt,
+		Name:      core.Name,
+		Price:     core.Price,
+		Satuan:    core.Satuan,
+	}
+}
+
+func FromCoreSlice(core []product.Core) []Product {
+	var artArray []Product
+	for key := range core {
+		artArray = append(artArray, FromCore(core[key]))
+	}
+	return artArray
 }
