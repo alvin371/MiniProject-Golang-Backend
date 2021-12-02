c1bfae6b052a44c7ad55424668263129a063fb60
alvinsatria31 <alvin.satria@konsultan.belajar.id>
2021-12-02 08:22:14 +0700

    update factory daily-menu

--- factory/factory.go
+++ factory/factory.go
@@ -2,6 +2,9 @@ package factory
 
 import (
 	"A-Golang-MiniProject/config"
+	_daily_bussiness "A-Golang-MiniProject/features/daily-menu/bussiness"
+	_daily_data "A-Golang-MiniProject/features/daily-menu/data"
+	_daily_presentation "A-Golang-MiniProject/features/daily-menu/presentation"
 	_product_bussiness "A-Golang-MiniProject/features/product/bussiness"
 	_product_data "A-Golang-MiniProject/features/product/data"
 	_product_presentation "A-Golang-MiniProject/features/product/presentation"
@@ -9,6 +12,7 @@ import (
 
 type Presenter struct {
 	ProductPresentation *_product_presentation.ProductHandler
+	DailyPresentation   *_daily_presentation.DailyMenuHandler
 }
 
 func Init() Presenter {
@@ -16,7 +20,12 @@ func Init() Presenter {
 	productBussiness := _product_bussiness.NewProductBussiness(productData)
 	productPresentation := _product_presentation.NewProductHandler(productBussiness)
 
+	dailyData := _daily_data.NewmysqlDailyMenuRepository(config.DB)
+	dailyBussiness := _daily_bussiness.NewDailyMenuBussiness(dailyData)
+	dailyPresentation := _daily_presentation.NewDMHandler(dailyBussiness)
+
 	return Presenter{
 		ProductPresentation: productPresentation,
+		DailyPresentation:   dailyPresentation,
 	}
 }
